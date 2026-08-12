605fb5bc3da332e59b1922950949130693956e2c
Rajan9865 <rajan78800@gmail.com>
2026-08-12 18:14:04 +0530

    application.properties

--- user-management-system/src/main/java/com/usermanagement/user_management_system/config/OpenApiConfig.java
+++ user-management-system/src/main/java/com/usermanagement/user_management_system/config/OpenApiConfig.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,24 +19,35 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class OpenApiConfig {
+
+    private static final String SECURITY_SCHEME_NAME = "bearerAuth";
+
     @Bean
     public OpenAPI userManagementOpenAPI() {
-return new OpenAPI().info(new Info().title("User Management System")
-.description("REST APIs for User Management System")
-.version("1.0").contact(new Contact()
-                        .name("Rajan kumar")
-                .email("rajan06166@gmail.com")
-                .url("https://www.linkedin.com/in/rajan-kumar-78800/"))
-        .license(new License()
-                .name("Apace 2.0")
-                .url("https://www.apache.org/licenses/LICENSE-2.0")))
-        .components(new Components()
-                .addSecuritySchemes("basicAuth",
-                        new SecurityScheme()
-                                .type(SecurityScheme.Type.HTTP)
-                                .scheme("basic")))
-        .externalDocs(new ExternalDocumentation()
-                .description("Project Documentation")
-                .url("https://github.com/Rajan9865/Practise_Project"));
+        return new OpenAPI()
+                .info(new Info()
+                        .title("User Management System")
+                        .description("REST APIs for User Management System")
+                        .version("1.0")
+                        .contact(new Contact()
+                                .name("Rajan kumar")
+                                .email("rajan06166@gmail.com")
+                                .url("https://www.linkedin.com/in/rajan-kumar-78800/"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("https://www.apache.org/licenses/LICENSE-2.0")))
+                // Apply JWT security globally to all endpoints
+                .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
+                .components(new Components()
+                        .addSecuritySchemes(SECURITY_SCHEME_NAME,
+                                new SecurityScheme()
+                                        .name(SECURITY_SCHEME_NAME)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .description("Provide the JWT access token obtained from POST /api/v1/auth/login")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("Project Documentation")
+                        .url("https://github.com/Rajan9865/Practise_Project"));
     }
 }
